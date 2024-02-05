64f89d255496f54000ac0a42f025e639843aec93
Young Lee <syounglee012@gmail.com>
2024-02-05 14:44:35 -0800

    hiGuys

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
-
 export default function Home() {
-  
-
   return (
     <>
-    <div className='text-xs'>
-
-      <h1>Home</h1>
-      <p>Welcome to the home page</p>
-    </div>
+      <div className="text-xs">
+        <h1>Home</h1>
+        <p>Welcome to the home page</p>
+        <p>Hi guys - YL</p>
+      </div>
     </>
   );
 }
